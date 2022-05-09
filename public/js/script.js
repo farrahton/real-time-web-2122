7494f357a4e591f723c4f238ce977c3a8408c12f
farrahton <farrahtonpiers@hotmail.com>
2022-05-10 01:13:04 +0200

    :construction: drama turn based  :construction:

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,7 @@
 let socket = io()
+let magSpelen = false;
+
+// require("dotenv").config()
 
 const card = document.querySelectorAll('.card')
 const front = document.querySelectorAll('.front')
@@ -6,69 +9,20 @@ const content = document.querySelectorAll('.content')
 const container = document.querySelector('main')
 const score = document.querySelector('span')
 
-// function playername() { 
-// let name = prompt("what's your name?", "John Doe")
-// if (name != null) {
-//     document.getElementById("name").innerHTML =
-//     "Hello " + name + "! How are you today?";
-//   }
-// }
-let playerName = prompt("what's your name?", "John Doe")
-socket.emit("player", playerName)
 
-socket.on("onlinePlayers", (onlinePlayers) => {
-    // Clear the list of current online connected users.
-    document.querySelector("ul").innerHTML = ""
-
-    onlinePlayers.forEach((onlinePlayer) => {
-        // Add the player to the list.
-        document.querySelector("ul").appendChild(Object.assign(document.createElement("li"), {
-            innerHTML: `${onlinePlayer[0]} <span id="playersscore">${onlinePlayer[2]}</span>`
-        }))
-    })
-})
-
-
-// to shuffle the cards beforehand 
+// shuffle de kaarten voorafgaand het spel
 card.forEach(c => {
     const num = [...Array(card.length).keys()]
     const random = Math.floor(Math.random() * card.length)
     c.style.order = num[random]
 })
 
+// user kan zijn/haar naam intypen
+let playerName = prompt("what's your name?", "John Doe")
+socket.emit("player", playerName)
+
+// voer de functie uit zodat de player even de kaartjes te zien krijgt 
 showCardsAtFirst()
-// clicking()
-
-
-
-
-
-
-//   socket.on('cardsScore', (cardsScore) => {
-//         users[socket.id].score = cardsScore;
-//         io.emit('cardsScore', users)
-//     })
-
-// socket.on("addScore", card => {
-
-// })
-
-
-
-
-// console.log('works')
-// socket.emit('flippedTwoCards', "hallo")
-
-// socket.emit('test', "Lisanne")
-
-// clicking()
-
-card.forEach(e => {
-    clicking()
-})
-
-// clicking()
-
 function showCardsAtFirst() {
     for (let i = 0; i < content.length; i++) {
         // show the cards a few seconds before
@@ -80,61 +34,99 @@ function showCardsAtFirst() {
         }, 2000);
     }
 }
-// when you click on any of the cards from the array it flips
-function clicking() {
-    for (let i = 0; i < content.length; i++) {
-        card[i].addEventListener('click', () => {
-            content[i].classList.add('flip')
-            // socket.emit("clickCard", i)
 
-            // without the time out it takes too long to see the scoreboard go up
-            setTimeout(() => {
-                const flippedCard = document.querySelectorAll('.flip')
+socket.on("onlinePlayers", (onlinePlayers) => {
+    // lege lijst van players die momenteel online zijn
+    document.querySelector("ul").innerHTML = ""
 
-                //  limit to amount of cards you can flip
-                if (flippedCard.length == 2) {
+    onlinePlayers.forEach((onlinePlayer) => {
+        // voeg de player (nummer 0 van de array) toe aan de lijst in html en geef die een startscore (nummer 2 in de array) van 0
+        document.querySelector("ul").appendChild(Object.assign(document.createElement("li"), {
+            innerHTML: `${onlinePlayer[0]} <span id="playersscore">${onlinePlayer[2]}</span>`
+        }))
+         // even scrollen als de lijst te lang wordt
+         document.querySelector("ul").scrollTop = document.querySelector("ul").scrollHeight
+    })
+})
 
-                    // then you're not allowed to do anything with your mouse anymore
-                    // container.style.pointerEvents = 'none'
 
-                    // // after 1 second you're allowed to again
-                    // setInterval(() => {
-                    //     container.style.pointerEvents = 'all'
-                    // }, 1000);
-                    console.log(flippedCard);
-                    match(flippedCard[0], flippedCard[1])
-                }
-            }, 500)
-        })
+// de flip animatie op de kaart wordt aan alle online players getoond
+socket.on("clickCard", card => {
+    content[card].classList.add('flip')
+})
+
+// keuze om op de knop te klikken om de beurt te geven aan de volgende player in de array
+// document.querySelector("#next-turn").addEventListener("click", (event) => {
+//     event.preventDefault()
+//     socket.emit("turn")
+// })
+
+socket.on("activePlayer", (activePlayer) => {
+    // console.log("playerName: " + playerName)
+    // console.log("activeplayer: " + activePlayer)
+    // console.log("activePlayer == playerName: " + (activePlayer == playerName))
+    if (activePlayer == playerName) {
+        console.log("Het is jouw beurt!")
+        magSpelen = true;
+
+        container.style.pointerEvents = 'all'
+        
+        for (let i = 0; i < content.length; i++) {
+            card[i].addEventListener('click', () => {
+                console.log("Test")
+                // content[i].classList.add('flip')
+                socket.emit("clickCard", i)
+    
+                // zonder de tijd duurde het te lang voor de score en kan je er opeens 3 aanklikken?
+                setTimeout(() => {
+                    const flippedCard = document.querySelectorAll('.flip')
+    
+                    // we willen alleen de lengte van de geflipte kaarten weten
+                    if (flippedCard.length == 2) {
+                        // zodra de lengte 2 is, dan mag je niets meer met je muis doen in het gebied van de kaarten
+                        container.style.pointerEvents = 'none'
+                        // en wordt de match functie uitgevoerd op de twee kaarten
+                        match(flippedCard[0], flippedCard[1])
+                        // de volgende player in de array krijgt de beurt
+                        socket.emit("turn")
+                    }
+                }, 500)
+            })
+        }
+
+    } else {
+        magSpelen = false;
+        // als je niet aan de beurt bent mag je niets met je muis doen in het gebied van de kaartjes
+        container.style.pointerEvents = 'none'
+        console.log("Jij mag nog geen kaarten omdraaien..");
+         // de volgende player in de array krijgt de beurt na 10 seconden dan switcht ie wel, maar pakt ie de length ==2 niet?
+        //  setTimeout(() => {
+        //   socket.emit("turn")
+    // }, 10000)
     }
-}
+})
+
 
 
 function match(cardOne, cardTwo) {
-    // if the data indexes match then 
+    // als de data-index matcht van de twee omgedraaide kaarten dan
     console.log(cardOne, cardTwo);
     if (cardOne.dataset.index == cardTwo.dataset.index) {
-        // add one point to the score
-        score.innerHTML = parseInt(score.innerHTML) + 1
+        // 1 punt toevoegen aan scoreboard 
+        // playersscore.innerHTML = parseInt(playersscore.innerHTML) + 1
+         // 1 punt toevoegen aan score van player
+        socket.emit("playerScore")
+        // console.log(playerScore)
 
+        // haal de flip class ervanaf
         cardOne.classList.remove('flip')
         cardTwo.classList.remove('flip')
 
-        // and keep showing the matched cards
+        // en voeg match toe om hem omgedraaid in beeld te laten 
         cardOne.classList.add('match')
         cardTwo.classList.add('match')
-
-        // // then you're not allowed to do anything with your mouse
-        // container.style.pointerEvents = 'none'
-
-        // after a second you can do things with your mouse again
-        // setInterval(() => {
-        //     container.style.pointerEvents = 'all'
-        // }, 1000);
     } else {
-        // if the cards do not match then you're not allowed to do anything with your mouse
-        // container.style.pointerEvents = 'none'
-
+        // als de kaarten niet matchen halen we de flip animatie ervanaf om weer de achterkant van de kaarten te zien na 1 seconde
         setTimeout(() => {
             cardOne.classList.remove('flip')
             cardTwo.classList.remove('flip')
@@ -142,13 +134,24 @@ function match(cardOne, cardTwo) {
     }
 }
 
+// socket.on("playerScore", playersscore => {
+//     playersscore.innerHTML = parseInt(playersscore.innerHTML) + 1
 
+// })
 
+// const APIKEY = process.env.API_KEY
 
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Host': 'healthruwords.p.rapidapi.com',
+// 		'X-RapidAPI-Key': '${APIKEY}'
+// 	}
+// };
 
-
-
-
-
+// fetch('https://healthruwords.p.rapidapi.com/v1/quotes/?t=Wisdom&maxR=1&size=medium&id=731', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
 
 
--- server.js
+++ server.js
@@ -4,36 +4,89 @@ const http = require('http').createServer(app)
 const path = require('path')
 const io = require('socket.io')(http)
 const port = process.env.PORT || 4500
-// const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 app.use(express.static(path.resolve('public')))
 
+// ejs instellen als template engine
+app.set('view engine', 'ejs')
+app.set('views', './views')
+
+// route voor de index
+app.get('/', function (req, res) {
+    res.render('index');
+  })
+
+// route voor de facts 
+app.get('/facts', async (req, res) => {
+    res.render('facts')
+})
+
+  
 let onlinePlayers = []
+// let activePlayer = "" 
+
+let counter = 0
+
+// console.log(counter)
 
 io.on('connection', (socket) => {
-    console.log('made a connection' + socket.id)
-
-    socket.on("player", playerName => {
-         // Add the name, connection ID and score to the list of online players.
-         onlinePlayers.push([playerName, socket.id, 0])
-
-         // Emit the names, connection IDs and scores of the online players.
+    socket.on("player", (playerName) => {
+        // voeg de prompt playername, socket id en de startscore aan de array toe
+        onlinePlayers.push([playerName, socket.id, 0])
+         // emit de prompt playername, socket id en de startscore
         io.emit("onlinePlayers", onlinePlayers)
+
+        io.emit("activePlayer", onlinePlayers[counter][0])
+    })
+
+    socket.on("turn", () => {
+        // als counter er 1 bij heeft moet het minder dan of gelijk zijn aan de lengte van de array
+        if (counter + 1 <= onlinePlayers.length - 1) {
+            counter++
+        } else {
+            counter = 0
+        }
+
+        // console.log(counter)
+
+        console.log(onlinePlayers[counter])
+         // put the [0] on server side already, zodat je minder dataverkeer hebt
+        io.emit("activePlayer", onlinePlayers[counter][0])
+    })
+    
+    socket.on('clickCard', card => {
+        io.emit("clickCard", card)
+    })
+
+
+    socket.on("playerScore", () => {
+    //     onlinePlayers.forEach((onlinePlayer, index) => {
+    
+    //         if (onlinePlayer[0] == socket.id) {
+    //             onlinePlayers[index][1]++
+    //         }
+    // })
+        io.emit("playerScore")
     })
 
     socket.on('disconnect', () => {
-        console.log('user disconnected')
-
-        // Remove disconnected players from list of online players!
+        // console.log('user disconnected')
+        // removed disconnecter player from list when you refresh
+        // onlinePlayers.splice(onlinePlayers.indexOf(socket),1);
+        // remove disconnected players from list of online players! 
+        // onlinePlayers.forEach((onlinePlayer, index) => {
+        //     if (onlinePlayer[1] == socket.id) {
+        //         onlinePlayers.splice(index, 1)
+        //     }
+        // })
+        // io.emit
     })
 })
-
 
 http.listen(port, () => {
     console.log('listening on port ', port)
 })
-
-
 
 
 
