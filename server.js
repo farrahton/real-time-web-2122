b83579c1b5994d62486db5ddca341262fa59d211
farrahton <farrahtonpiers@hotmail.com>
2022-05-12 19:24:01 +0200

    :building_construction: :children_crossing: fetch server side

--- server.js
+++ server.js
@@ -4,61 +4,54 @@ const http = require('http').createServer(app)
 const path = require('path')
 const io = require('socket.io')(http)
 const port = process.env.PORT || 4500
-// const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+
 
 app.use(express.static(path.resolve('public')))
 
 // ejs instellen als template engine
-app.set('view engine', 'ejs')
-app.set('views', './views')
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 
 // route voor de index
 app.get('/', function (req, res) {
-    res.render('index');
+    res.render('index')
   })
 
 // route voor de facts 
-app.get('/facts', async (req, res) => {
-    res.render('facts')
-    // fetch("https://black-history-month-api.herokuapp.com/people")
-    // .then(function(response) {
-    //   return response.json();
-    // })
-    // .then(function(data) {
-    //   var data = data[Math.floor(Math.random()*data.length)];
-      
-    //   console.log(data);
-      // data[288]
-      // console.log(data[288]);
-    //   res.render('facts', {
-        // data: data.description
-    //   })
-    // })
-    // en dan in ejs :  <!-- <% data((facts)=> { %>
-            //     <blockquote> 
-            //     <%= facts.description %> 
-            // </blockquote> 
-            //  <% }) %> -->
+app.get('/facts', function (req, res, next){
+    // res.render('facts')
+    fetch('https://black-history-month-api.herokuapp.com/people')
+    .then(function(response) {
+      return response.json()
+    })
+    .then(function(data) {
+      var data = data[Math.floor(Math.random()*data.length)]
+    //   console.log(data)
+      data[288]
+    //   console.log(data[288])
+      res.render('facts', {
+        fact: data.description
+      })
+    })
+  
 })
 
 let onlinePlayers = []
-// let activePlayer = "" 
 
 let counter = 0
 
-// console.log(counter)
-
 io.on('connection', (socket) => {
-    socket.on("player", (playerName) => {
+    socket.on('player', (playerName) => {
         // voeg de prompt playername, socket id aan de array toe
         onlinePlayers.push([playerName, socket.id])
          // emit de prompt playername, socket id
-        
-        io.emit("onlinePlayers", onlinePlayers)
-        io.emit("activePlayer", onlinePlayers[counter][1])
+        io.emit('onlinePlayers', onlinePlayers)
+        io.emit('activePlayer', onlinePlayers[counter][1])
     })
 
-    socket.on("turn", () => {
+    socket.on('turn', () => {
         // als we er 1 aan toevoegen past het dan? dan mag je er 1 aan toevoegen
         // lengte begint bij 1 daarom - 1 we willen dat ie bij 0 begint, omdat eerste in de lijst index 0 heeft
         if (counter + 1 <= onlinePlayers.length - 1) {
@@ -69,14 +62,14 @@ io.on('connection', (socket) => {
             counter = 0
         }
         // console.log(counter)
-        console.log('De beurt is aan:', onlinePlayers[counter][1])
-         // benoem de [0] alvast server side, zodat je minder dataverkeer hebt
-        io.emit("activePlayer", onlinePlayers[counter][1])
+        // console.log('De beurt is aan:', onlinePlayers[counter][1])
+         // benoem de alvast server side, zodat je minder dataverkeer hebt
+        io.emit('activePlayer', onlinePlayers[counter][1])
     })
     
     // zodat je de geklikte kaart op allebei de players hun scherm ziet
     socket.on('clickCard', card => {
-        io.emit("clickCard", card)
+        io.emit('clickCard', card)
     })
 
     socket.on('disconnect', () => {
